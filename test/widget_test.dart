6bc5b2aa4964b3559674907f334a69dfd4e3eecc
Thukten-Norbu999 <thuktennorbu33@gmail.com>
2022-05-05 20:12:51 +0600

    Not Much

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:druk_pizza/lib/main.dart';
+import 'package:druk_pizza/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
